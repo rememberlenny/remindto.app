8843d33ffe2946a3ebd0a8c659ddf89b0a76f417
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-12-03 20:40:54 -0500

    Add the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_234914) do
+ActiveRecord::Schema.define(version: 2019_12_04_014017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,13 @@ ActiveRecord::Schema.define(version: 2019_10_27_234914) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["provider"], name: "index_authentications_on_provider"
     t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
+  create_table "email_templates", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "laters", force: :cascade do |t|
