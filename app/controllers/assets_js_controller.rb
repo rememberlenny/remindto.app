1cf2275fb368e974b33bdeca002eacb0afdd0883
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-01-09 15:19:00 -0500

    Save the change on your account

--- app/controllers/assets_js_controller.rb
+++ app/controllers/assets_js_controller.rb
@@ -3,14 +3,26 @@ class AssetsJsController < ApplicationController
   skip_before_action :authenticate_user!
   
   def remind
-    @target_el = params[:target]
-    @startOpen = params[:startOpen]
-    @shouldBeInline = params[:shouldBeInline]
-    @wasSent = params[:wasSent]
+    @account_uid = assets_params[:id]
+    if @account_uid && assets_params.keys.count == 1
+      @target_el = 'ReadturnEmbedTarget'
+      @startOpen = false
+      @shouldBeInline = false
+      @wasSent = false
+    else
+      @target_el = assets_params[:target] ? assets_params[:target] : 'ReadturnEmbedTarget'
+      @startOpen = assets_params[:startOpen] ? assets_params[:startOpen] : false
+      @shouldBeInline = assets_params[:shouldBeInline] ? assets_params[:shouldBeInline] : false
+      @wasSent = assets_params[:wasSent] ? assets_params[:wasSent] : false
+    end
     respond_to do |format|
       format.js {
         render :file => "assets_js/bundle_wrapper.js.erb"
       }
     end
+  end
+
+  def assets_params
+    params.permit(:id, :target, :startOpen, :shouldBeInline, :wasSent)
   end
 end
